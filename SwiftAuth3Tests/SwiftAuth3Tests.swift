2ad33994969fffd14086994766b237e17705ef09
silaaskin <silaaskin0141@gmail.com>
2026-03-19 15:39:42 +0300

    Initial Commit

---
+++ SwiftAuth3Tests/SwiftAuth3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAuth3Tests.swift
+//  SwiftAuth3Tests
+//
+//  Created by Sıla Aşkın on 19.03.2026.
+//
+
+import Testing
+@testable import SwiftAuth3
+
+struct SwiftAuth3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
